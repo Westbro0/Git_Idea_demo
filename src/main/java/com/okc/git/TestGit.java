afae18959fc8518119221c15b87ed7f2c9ede91d
Cindy <Cindy@ins.com>
2021-08-28 17:36:04 +0800

    hot-fix conflict commit

--- src/main/java/com/okc/git/TestGit.java
+++ src/main/java/com/okc/git/TestGit.java
@@ -4,6 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("How are you?");
-        System.out.println("Good Luck!!!");
+        System.out.println("Good Luck to you!!!");
     }
 }
