574f27c8d8a619e27d6eceeced12f7f7d941781e
Cindy <Cindy@ins.com>
2021-08-28 17:16:26 +0800

    hot-fix commit

--- src/main/java/com/okc/git/TestGit.java
+++ src/main/java/com/okc/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("How are you?");
+        System.out.println("Good Luck!!!");
     }
 }
